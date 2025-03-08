12ebe65a4bd3ac1b41a8eb052d77e5c26f5eabc1
Denard Soares <denards@hotmail.com>
2025-03-08 00:28:10 +0000

    Copilot second commit

---
+++ members.js
@@ -0,0 +1,6 @@
+function skillsMembers() {
+    return [
+        { name: 'John', skills: ['JavaScript', 'React', 'Node'] },
+        { name: 'Jane', skills: ['JavaScript', 'Angular', 'Python'] }
+    ];
+}
